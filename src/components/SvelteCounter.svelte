6e39ce6b501f2146cf78a78407332c677d0c505c
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-03-23 17:39:21 +0100

    Initial commit

---
+++ src/components/SvelteCounter.svelte
@@ -0,0 +1,24 @@
+<script>
+  let count = 0;
+
+  function add() {
+    count += 1;
+  }
+
+  function subtract() {
+    count -= 1;
+  }
+</script>
+
+<div id="svelte" class="counter">
+  <button on:click={subtract}>-</button>
+  <pre>{ count }</pre>
+  <button on:click={add}>+</button>
+</div>
+
+<style>
+  .counter {
+    color: #333;
+    background: pink;
+  }
+</style>
